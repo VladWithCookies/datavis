7afc7aeaa30cc389a8ccf6896f69f2b033c40367
Vlad Viderko <summervibeswith9pm@gmail.com>
2022-10-14 17:13:40 +0200

    Add legend to pie chart

--- src/pages/PieChart/PieChart.js
+++ src/pages/PieChart/PieChart.js
@@ -1,60 +1,75 @@
-import { prop } from 'ramda';
-import { pie, arc } from 'd3';
+import { prop, map } from 'ramda';
+import { scaleOrdinal, pie, arc } from 'd3';
 
 export default function PieChart() {
-  const height = 450;
-  const width = 450;
+  const height = 800;
+  const width = 800;
 
   const margin = {
     top: 20,
-    right: 20,
+    right: 300,
     bottom: 20,
     left: 20,
   };
 
-  const centerX = width / 2;
-  const centerY = height / 2;
-  const radius = Math.min(width, height) / 2 - margin.top - margin.right;
+  const innerHight = height - margin.top - margin.bottom;
+  const innerWidth = height - margin.left - margin.right;
+  const radius = Math.min(innerHight, innerWidth) / 2;
 
-  const rawData = [
-    {
-      value: 75,
-      label: 'Sky',
-      color: 'blue',
-    },
-    {
-      value: 15,
-      label: 'Sunny Side of Pyramid',
-      color: 'yellow'
-    },
-    {
-      value: 10,
-      label: 'Shady Side of Pyramid',
-      color: 'brown',
-    },
+  const data = [
+    { value: 75, label: 'Sky' },
+    { value: 15, label: 'Sunny side of the pyramid' },
+    { value: 10, label: 'Shady side of the pyramid' },
   ];
+
+  const colorValue = prop('label');
+
+  const colorScale = scaleOrdinal()
+    .domain(map(colorValue, data))
+    .range(['blue', 'yellow', 'brown']);
 
   const createPie = pie()
     .value(prop('value'));
 
+  const angleOffset = - 3 * Math.PI / 4;
+
   const createArc = arc()
     .innerRadius(0)
-    .outerRadius(radius);
-
-  const data = createPie(rawData);
+    .outerRadius(radius)
+    .startAngle(({ startAngle }) => startAngle + angleOffset)
+    .endAngle(({ endAngle }) => endAngle + angleOffset);
 
   return (
     <svg
       height={height}
       width={width}
     >
-      <g transform={`translate(${centerX}, ${centerY}) rotate(-135)`}>
-        {data.map((item, index) => (
+      <g transform={`translate(${radius + margin.left}, ${radius + margin.top})`}>
+        {createPie(data).map((item) => (
           <path
             key={item.data.label}
             d={createArc(item)}
-            fill={item.data.color}
+            fill={colorScale(colorValue(item.data))}
           />
+        ))}
+      </g>
+      <g transform={`translate(${innerWidth + 80}, ${margin.top + 40})`}>
+        {colorScale.domain().map((item, index) => (
+          <g
+            key={item}
+            transform={`translate(0, ${index * 30})`}
+          >
+            <circle
+              r={10}
+              fill={colorScale(item)}
+            />
+            <text
+              x={20}
+              dy="0.32em"
+            >
+              {item}
+            </text>
+          </g>
         ))}
       </g>
     </svg>
